52bb53c25a10ab1c231050614ae595c8516fca00
Saran <saranvms2002@gmail.com>
2025-05-08 15:18:54 +0530

    new3

--- project/settings.py
+++ project/settings.py
@@ -71,22 +71,15 @@ WSGI_APPLICATION = 'project.wsgi.application'
 import dj_database_url
 import os
 
-DATABASE_URL = os.environ.get('DATABASE_URL')
+import dj_database_url
 
-if DATABASE_URL:
-    DATABASES = {
-        'default': dj_database_url.config(
-            conn_max_age=600,
-            ssl_require=True
-        )
-    }
-else:
-    DATABASES = {
-        'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
-        }
-    }
+DATABASES = {
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600,
+        ssl_require=True
+    )
+}
 
 # Password validation
 AUTH_PASSWORD_VALIDATORS = [
