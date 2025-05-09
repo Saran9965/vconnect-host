71b2e38a946896605ff51409efce16102f42cdba
Saran <saranvms2002@gmail.com>
2025-05-09 12:51:51 +0530

    changedfully

--- project/settings.py
+++ project/settings.py
@@ -81,6 +81,13 @@ DATABASES = {
     }
 }
 
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / "db.sqlite3",
+#     }
+# }
+
 # import os
 # import dj_database_url
 # from dotenv import load_dotenv
